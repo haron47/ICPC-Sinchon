fa309fb60aec94450ad7e13f087b3fd6376357c1
haron47 <66551410+haron47@users.noreply.github.com>
2021-09-18 03:26:44 +0900

    2회차

--- [2회차] 시간복잡도, Sorting/_11582.java
+++ [2회차] 시간복잡도, Sorting/_11582.java
@@ -1,52 +1,22 @@
-import java.util.*;
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class _11582 {
-	public static int[] arr;
-	public static int[] tmp;
-	public static int k;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		arr = new int[N];
-		
-		for(int i = 0; i < N; i++)
-			arr[i] = sc.nextInt();
-		
-		k = sc.nextInt();
-		mergeSort(0, arr.length -1, 0);
-		
-		for(int i = 0; i < N; i++)
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i += N / k)
+			Arrays.sort(arr, i, i + N / k);
+		for (int i = 0; i < N; i++)
 			System.out.print(arr[i] + " ");
-		sc.close();
-	}
-
-	public static void mergeSort(int start, int end, int sum) {
-		System.out.println(sum);
-		for(int i = 0; i < arr.length; i++)
-			System.out.print(arr[i] + " ");
-		if(sum > k)
-			return;
-		if (start < end) {
-			sum += 1;
-			int mid = (start + end) / 2;
-			mergeSort(start, mid, sum);
-			mergeSort(mid + 1, end, sum);
-
-			int p = start;
-			int q = mid + 1;
-			int idx = p;
-
-			while (p <= mid || q <= end) {
-				if (q > end || (p <= mid && arr[p] < arr[q])) {
-					tmp[idx++] = arr[p++];
-				} else {
-					tmp[idx++] = arr[q++];
-				}
-			}
-			for (int i = start; i <= end; i++) {
-				arr[i] = tmp[i];
-			}
-		}
 	}
 }
